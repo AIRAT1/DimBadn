85d77dc37557e02fb49038978277c1a260b31be0
Ayrat <ayrat1@mail.ru>
2017-04-20 23:48:36 +0300

    add crashlytics from fabric

--- app/src/main/java/de/android/dimbadn/MainActivity.java
+++ app/src/main/java/de/android/dimbadn/MainActivity.java
@@ -2,12 +2,15 @@ package de.android.dimbadn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
 
         if (getFragmentManager().findFragmentById(android.R.id.content) == null) {
             getFragmentManager().beginTransaction()
